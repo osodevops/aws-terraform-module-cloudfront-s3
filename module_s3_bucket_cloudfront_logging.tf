7d5e4d410534b971cc4201040789280d0eca3467
MrDevopsman <joshuacallis@icloud.com>
2026-06-10 12:49:55 +0100

    format

--- module_s3_bucket_cloudfront_logging.tf
+++ module_s3_bucket_cloudfront_logging.tf
@@ -2,12 +2,12 @@ module "bucket_cloudwatch_logs_backup" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "~>4.0"
 
-  bucket                   = local.logging_bucket_name
-  force_destroy            = false
-  tags                     = var.common_tags
-  acl                      = var.whitelabel_domain || var.acl_disabled ? null : "private"
-  object_ownership         = "ObjectWriter"
-  control_object_ownership = var.whitelabel_domain || var.owner_enabled ? true : false
+  bucket                            = local.logging_bucket_name
+  force_destroy                     = false
+  tags                              = var.common_tags
+  acl                               = var.whitelabel_domain || var.acl_disabled ? null : "private"
+  object_ownership                  = "ObjectWriter"
+  control_object_ownership          = var.whitelabel_domain || var.owner_enabled ? true : false
   attach_access_log_delivery_policy = var.whitelabel_domain ? true : false
 
   # Bucket public access
